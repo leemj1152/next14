7ff2ca1a74f0cedd76936650a15b1f1881520a45
Home <lee3321998@naver.com>
2024-02-02 22:04:49 +0900

    Dynamic Metadata

--- components/movie-info.jsx
+++ components/movie-info.jsx
@@ -1,11 +1,28 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
-async function getMovie(id) {
+export async function getMovie(id) {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
 
 export default async function MovieInfo({ id }) {
-  const Movie = await getMovie(id);
-  return <h6>{JSON.stringify(Movie)}</h6>;
+  const movie = await getMovie(id);
+  return (
+    <div className={styles.container}>
+      <img
+        className={styles.poster}
+        src={movie.poster_path}
+        alt={movie.title}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>* {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
